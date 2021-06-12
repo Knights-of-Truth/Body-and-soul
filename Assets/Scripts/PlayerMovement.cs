3ec776b6085609de2a344cb8f48c84624fd89aa1
RedWn <redwaniscool@gmail.com>
2021-06-12 11:29:26 +0300

    Added Tiles and Tilemaps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Animator anim;
+    //public Animator anim;
     public float movementSpeed;
     public Rigidbody2D rb;
     float dx;
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     bool Dashed = false;
     bool isDashing;
     public int state;
-    public float dashDist = 5f;
+    public float dashDist;
     private void Start() {
     
     }
@@ -26,9 +26,9 @@ public class PlayerMovement : MonoBehaviour
             }
         
         if (Mathf.Abs(dx) > 0.05){
-            anim.SetBool("IsRunning", true);
+            //anim.SetBool("IsRunning", true);
         }else{
-            anim.SetBool("IsRunning", false);
+            //anim.SetBool("IsRunning", false);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && ( state == 1 || state == 2)){
@@ -40,9 +40,14 @@ public class PlayerMovement : MonoBehaviour
         }
     }
     private void FixedUpdate() {
+<<<<<<< Updated upstream
         dx=Input.GetAxisRaw("Horizontal");
+=======
+        if (!isDashing){
+>>>>>>> Stashed changes
         Vector2 movement = new Vector2(dx * movementSpeed, rb.velocity.y);
         rb.velocity = movement;
+        }
     }
     void Jump(){
         Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
@@ -50,22 +55,24 @@ public class PlayerMovement : MonoBehaviour
     }
     bool isGrounded(Transform temp){
          Collider2D groundCheck = Physics2D.OverlapCircle(temp.position, 0.5f, groundLayers);
-         if (groundCheck != null)
+         if (groundCheck != null){
+            Dashed = false;
             return true;
+         }
         return false;
     }
 
     IEnumerator Dash (float direction){
         isDashing = true;
         Dashed = true;
-        anim.SetBool("IsDashing", isDashing);
+        //anim.SetBool("IsDashing", isDashing);
         rb.velocity = new Vector2(rb.velocity.x, 0f);
         rb.AddForce(new Vector2(dashDist * direction, 0f), ForceMode2D.Impulse);
         float gravity = rb.gravityScale;
         rb.gravityScale = 0;
         yield return new WaitForSeconds(0.2f);
         isDashing = false;
-        anim.SetBool("IsDashing", isDashing);
+        //anim.SetBool("IsDashing", isDashing);
         rb.gravityScale = gravity;
     }
 }
