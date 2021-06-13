0fb2431e0abada272f5c01ac810eeb814d991835
IyadAlanssary <iyadalanssary@outlook.com>
2021-06-13 12:20:13 +0300

    added level 0 and some music tracks

---
+++ Assets/MusicClass.cs
@@ -0,0 +1,22 @@
+ using UnityEngine;
+ 
+ public class MusicClass : MonoBehaviour
+ {
+     public AudioSource _audioSource;
+     private void Awake()
+     {
+         DontDestroyOnLoad(transform.gameObject);
+         _audioSource = GetComponent<AudioSource>();
+     }
+ 
+     public void PlayMusic()
+     {
+         if (_audioSource.isPlaying) return;
+         _audioSource.Play();
+     }
+ 
+     public void StopMusic()
+     {
+         _audioSource.Stop();
+     }
+ }
